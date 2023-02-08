cbb672b013df888f7c9e0fb6ba25f2e7d09c3d86
Piotr Żywień <piotr.zywien@gmail.com>
2023-02-08 18:52:07 +0100

    axios requests to endpoints

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,22 +1,45 @@
 import React, { useState, useCallback, useEffect } from 'react'
 
+import Requests from '../../requests';
 import Tree from '../tree';
-import { NodeShape } from '../node';
+import { NodeShape, EmptyNode } from '../node';
 import { TreeShape } from '../tree';
 import FamilyTree from './FamilyTree';
 
 
 const App = () => {
-  const [nodes, setNodes] = useState<NodeShape[]>(FamilyTree.nodes);
-  const [tree, setTree] = useState<TreeShape>(FamilyTree.tree);
-  const [lastId, setLastId] = useState<number>(FamilyTree.index);
+  const [lastId, setLastId] = useState<number>(0);
+  const [nodes, setNodes] = useState<NodeShape[]>([EmptyNode(lastId, 0)]);
+  const [tree, setTree] = useState<TreeShape>({ id: lastId });
 
-  const setNodesCallback = useCallback((value: NodeShape[]) => setNodes(value), [nodes]);
-  const setTreeCallback = useCallback((value: TreeShape) => setTree(value), [tree]);
-  const setLastIdCallback = useCallback((value: number) => setLastId(value), [lastId]);
+  const setNodesCallback = useCallback((value: NodeShape[]) => setNodes(value), [lastId, nodes, tree]);
+  const setTreeCallback = useCallback((value: TreeShape) => setTree(value), [lastId, nodes, tree]);
+  const setLastIdCallback = useCallback((value: number) => setLastId(value), [lastId, nodes, tree]);
 
-  useEffect(() => {}, [nodes, tree, lastId]);
+  useEffect(() => {
+    Promise.all([
+      Requests.getIndex(),
+      Requests.getNodes(),
+      Requests.getTree(),
+    ]).then(([
+      index,
+      nodes,
+      tree,
+    ]) => {
+      setLastId(index);
+      setNodesCallback([...nodes]);
+      setTreeCallback({...tree});
+    });
+  }, []);
 
+  // Requests.save({
+  //   index: lastId,
+  //   nodes,
+  //   tree,
+  // });
+
+  // if (lastId === null || nodes === null || tree === null) return null;
+console.log(lastId, nodes, tree);
   return (
     <Tree
       nodes={nodes}
---
+++ src/requests/index.ts
@@ -0,0 +1,39 @@
+import axios from 'axios'
+
+import { NodeShape } from '../components/node';
+import { TreeShape } from '../components/tree';
+
+
+const HOST = 'http://localhost:2011/veo';
+
+const Resource = axios.create({
+  baseURL: HOST,
+});
+
+const getIndex = () => Resource.get(
+  '/getIndex',
+).then(({ data }) => data);
+
+const getNodes = () => Resource.get(
+  '/getNodes',
+).then(({ data }) => data);
+
+const getTree = () => Resource.get(
+  '/getTree',
+).then(({ data }) => data);
+
+const save = (payload: {
+  index?: number,
+  nodes?: NodeShape[],
+  tree?: TreeShape,
+}) => Resource.post(
+  '/save',
+  payload,
+).then(({ data }) => data);
+
+export default {
+  getIndex,
+  getNodes,
+  getTree,
+  save,
+};
